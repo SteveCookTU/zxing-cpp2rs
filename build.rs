b6719f6d67c350277546de1d55f708ba2fb24fce
Steve Cook <steviecook210@gmail.com>
2024-01-06 02:30:44 -0500

    Fix debug building on Windows

--- build.rs
+++ build.rs
@@ -1,7 +1,12 @@
 fn main() -> miette::Result<()> {
-    let dst = cmake::Config::new("zxing-cpp")
-        .define("BUILD_EXAMPLES", "OFF")
-        .build();
+    let mut config = cmake::Config::new("zxing-cpp");
+
+    config.define("BUILD_EXAMPLES", "OFF");
+
+    #[cfg(target_os = "windows")]
+    config.define("CMAKE_MSVC_RUNTIME_LIBRARY", "MultiThreadedDLL");
+
+    let dst = config.build();
 
     println!("cargo:rustc-link-search=native={}\\lib", dst.display());
     println!("cargo:rustc-link-lib=static=ZXing");
@@ -10,8 +15,14 @@ fn main() -> miette::Result<()> {
     let mut b = autocxx_build::Builder::new("src/lib.rs", &[&path])
         .extra_clang_args(&["-std=c++17", "-Wc++17-extensions"])
         .build()?;
-    b.flag_if_supported("-std:c++17")
-        .flag_if_supported("-Wc++17-extensions")
+
+    #[cfg(target_os = "windows")]
+    b.flag_if_supported("-std:c++17");
+
+    #[cfg(not(target_os = "windows"))]
+    b.flag_if_supported("-std=c++17");
+
+    b.flag_if_supported("-Wc++17-extensions")
         .compile("zxing-cpp2rs");
 
     println!("cargo:rerun-if-changed=src/lib.rs");
